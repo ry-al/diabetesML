6546bc1f867f2078c219325931cb84c4a58181db
ry-al <rylaalmario@gmail.com>
2020-08-07 22:39:14 -0400

    Add flask codes

--- app.py
+++ app.py
@@ -1,1 +1,58 @@
-# app
+from joblib import load
+import json
+from flask import Flask, jsonify, render_template, url_for
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+
+#################################################
+# HTML ROUTES
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/treatment")
+def profile():
+    return render_template("treatment.html")
+
+@app.route("/test.html")
+def charts():
+    return render_template("test.html")
+    
+@app.route("/test", methods=["POST", "GET"])
+def test():
+    # read data, do for each question, make sure the features are in correct order
+    age = request.form['age']
+    age = request.form['age']
+    age = request.form['age']
+    features = [age, x, x, x]
+    
+    
+    # load model
+    model = load("cancer_model.joblib")
+    prediction = model.predict(features)
+    # result is a 1 or 0 
+
+    if prediciton == 1, diabetes
+    else 
+
+    return render_template ("prediction.html", prediction)
+
+
+@app.route("/resources")
+def bar():
+    return render_template("resources.html")
+
+
+
+
