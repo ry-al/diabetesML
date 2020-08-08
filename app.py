1bea23136ce271caaf0db6783724fdeeaa8e29a3
ry-al <rylaalmario@gmail.com>
2020-08-08 13:14:00 -0400

    Merge branch 'master' of https://github.com/ry-al/diabetesML into rda/htmlfiles

--- app.py
+++ app.py
@@ -29,31 +29,65 @@ def profile():
 def charts():
     return render_template("test.html")
     
-@app.route("/test", methods=["POST", "GET"])
-def test1():
-    # read data, do for each question, make sure the features are in correct order
-    age1 = request.form['age1']
-    height1 = request.form['height1']
-    weight1 = request.form['weight1']
-    bmi = (703 * weight1)/(height1^2)
-    features = [age1, bmi]
+# @app.route("/test", methods=["POST", "GET"])
+# def simpleTest():
+#     # read data, do for each question, make sure the features are in correct order
+#     age1 = request.form['age1']
+#     height1 = request.form['height1']
+#     weight1 = request.form['weight1']
+
+#     # make sure that height is not 0
+#     if height >= 0:
+#         bmi = (703 * weight1)/(height1^2)
+#     else
+#         break
     
-    # load model
-    model = load("cancer_model.joblib")
-    prediction = model.predict(features)
-    # result is a 1 or 0 
+#     # user input list
+#     features = [age1, bmi]
+    
+#     # load model
+#     model = load("diabetes_model.joblib")
+#     prediction = model.predict(features)
+#     # result is a 1 or 0 
 
-    if prediction == 1:
-        # print  
-    else 
-        # print in html
+#     if prediction == 1:
+#         # print  
+#     else 
+#         # print in html
 
-    return render_template ("prediction.html", prediction)
+#     return render_template ("prediction.html", prediction)
 
+# @app.route("/test", methods=["POST", "GET"])
+# def Test():
+#     # read data, do for each question, make sure the features are in correct order
+#     age1 = request.form['age1']
+#     height1 = request.form['height1']
+#     weight1 = request.form['weight1']
 
-@app.route("/resources")
-def bar():
-    return render_template("resources.html")
+#     # make sure that height is not 0
+#     if height >= 0:
+#         bmi = (703 * weight1)/(height1^2)
+#     else
+#         break
+    
+#     # user input list
+#     features = [age1, bmi]
+    
+#     # load model
+#     model = load("diabetes_model.joblib")
+#     prediction = model.predict(features)
+#     # result is a 1 or 0 
+
+#     if prediction == 1:
+#         # print  
+#     else 
+#         # print in html
+
+#     return render_template ("prediction.html", prediction)
+
+@app.route("/analytics")
+def analytics():
+    return render_template("analytics.html")
 
 
 
